344ae5051c33a7f69c0f1681af5e7550da4ab5b5
helmesjo <helmesjo@gmail.com>
2026-05-18 20:53:15 +0200

    libangelscript: Update buildfiles. - Use FFmpeg-style MASM (assembly) pattern rules with explicit source dependencies

--- libangelscript/tests/basics/driver.cpp
+++ libangelscript/tests/basics/driver.cpp
@@ -1,30 +1,11 @@
-#include <sstream>
-#include <stdexcept>
-
-#include <angelscript.hpp>
+#include <angelscript.h>
 
 #undef NDEBUG
 #include <cassert>
 
-int main() {
-  using namespace std;
-  using namespace angelscript;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello(o, "World");
-    assert(o.str() == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try {
-    ostringstream o;
-    say_hello(o, "");
-    assert(false);
-  } catch (const invalid_argument &e) {
-    assert(e.what() == string("empty name"));
-  }
+int main ()
+{
+  asIScriptEngine* engine = asCreateScriptEngine ();
+  assert (engine != nullptr);
+  engine->ShutDownAndRelease ();
 }
